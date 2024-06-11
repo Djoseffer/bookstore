29c665a38f7b928a3fd4be4dff7f17185b489da1
Djoseffer <tom_djosefer@hotmail.com>
2024-06-10 22:10:44 -0300

    feat: bookservice started

---
+++ src/main/java/com/bookstore/services/BookService.java
@@ -0,0 +1,44 @@
+package com.bookstore.services;
+
+import com.bookstore.dtos.BookRecordDto;
+import com.bookstore.models.BookModel;
+import com.bookstore.models.ReviewModel;
+import com.bookstore.repositories.AuthorRepository;
+import com.bookstore.repositories.BookRepository;
+import com.bookstore.repositories.PublisherRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+@Service
+public class BookService {
+
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Transactional
+    public BookModel saveBook(BookRecordDto bookRecordDto){
+        BookModel book = new BookModel();
+        book.setTitle(bookRecordDto.title());
+        book.setPublisher(publisherRepository.findById(bookRecordDto.publisherId()).get());
+        book.setAuthors(authorRepository.findAllById(bookRecordDto.authorIds()).stream().collect(Collectors.toSet()));
+
+        ReviewModel reviewModel = new ReviewModel();
+        reviewModel.setComment(bookRecordDto.reviewComment());
+        reviewModel.setBook(book);
+        book.setReview(reviewModel);
+
+        return bookRepository.save(book);
+    }
+}
